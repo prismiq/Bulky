52d147a39c748bbbb51de94198e2cc6e9ac7025b
Jason Parker <>
2024-01-15 14:35:54 +0000

    main

--- Bulky/Data/ApplicationDbContext.cs
+++ Bulky/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace Bulky.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers {get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
