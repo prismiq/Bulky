e34bcc48f09ad55ed8ab077ed5ab94d21042c094
Jason Parker <>
2024-01-15 14:29:53 +0000

    Identity Framework Added

--- Bulky/Repository/IRepository/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-namespace Bulky.Repository.IRepository {
-    public interface IUnitOfWork {
-        ICategoryRepository Category {get; }
-        IProductRepository Product {get; }
-
-        void Save();
-    }
-}
